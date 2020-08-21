cd06613cf1e3f6029d15aa3772e431a0dda22c1f
Nicolas Epstein <nico@nilueps.net>
2020-08-21 10:05:29 -0400

    Updates

---
+++ precache-manifest.d698d4b09ad647996d7b5fc83ef99c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e7b66d209b4d339923e94f58070f5c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0bdfe7710dcc77bb0d77",
+    "url": "/tic-tac-toe/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "7221b335822194ba224e",
+    "url": "/tic-tac-toe/static/css/main.04847d98.chunk.css"
+  },
+  {
+    "revision": "0bdfe7710dcc77bb0d77",
+    "url": "/tic-tac-toe/static/js/2.937d4b1c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.937d4b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7221b335822194ba224e",
+    "url": "/tic-tac-toe/static/js/main.1d809f2a.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
